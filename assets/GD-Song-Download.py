b2cb223eb9af0fcbf21281798d2e5161710e1bde
sastofficial <39431922+sastofficial@users.noreply.github.com>
2022-01-28 14:14:49 +0100

    Update GD-Song-Download.py

--- assets/GD-Song-Download.py
+++ assets/GD-Song-Download.py
@@ -6,7 +6,7 @@ error = "Error! Error Code = "
 id = input("Enter LevelID: ")
 gd = get('https://gdbrowser.com/api/level/' + id)
 songlink = gd.json()['songLink']
-download = input("Do you want the song to open in browser.(Y/N): ")
+download = input("Do you want the song to open in browser?(Y/N): ")
 if download == "Y":
  webbrowser.open(songlink)
  print(songlink)
